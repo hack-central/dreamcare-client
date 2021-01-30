b910faded6dd45f3a87c84122bd7b8adeed3d0c8
Kartik Choudhary <kartikc.918@gmail.com>
2021-01-31 01:49:14 +0530

    Setup CRA, add prettier , add dir structure

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+// import Landing from './views/Landing';
+import Home from './views/Home';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="main">
+      <Router>
+        <Switch>
+          <Route exact path="/home">
+            <Home />
+          </Route>
+          <Route exact path="/">
+            {/* <Landing /> */}
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
