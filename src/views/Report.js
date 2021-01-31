8374205688183afaddd055a47dd7f936d864f7d0
Kartik Choudhary <kartikc.918@gmail.com>
2021-01-31 19:29:54 +0530

    Add report page, create week charts

--- src/views/Report.js
+++ src/views/Report.js
@@ -1,5 +1,9 @@
-// import 'tailwindcss/tailwind.css';
+import { Card, Row, Col } from 'antd';
+
+import HappinessChart from '../components/HappinessChart';
 import MoodCard from '../components/MoodCard';
+import MoodRadarChart from '../components/MoodRadarChart';
+import DiaryChart from '../components/DiaryChart';
 
 export default function Report() {
   return (
@@ -8,6 +12,28 @@ export default function Report() {
         <div className="daily-stats-card">...</div>
         <div className="daily-stats-card">
           <MoodCard />
+        </div>
+      </div>
+      <div className="daily-stats">
+        <div className="weekly-stats-card">
+          <Row>
+            <h1>Your Weekly DreamCare Statistics</h1>
+            <h6>(Placeholder data for the purpose of the hack)</h6>
+          </Row>
+          <Row justify="center">
+            <Col>
+              <MoodRadarChart />
+              Overall Mood Radar Chart
+            </Col>
+            <Col>
+              <HappinessChart />
+              Weekly Happiness Chart
+            </Col>
+            <Col>
+              <DiaryChart />
+              Dream Journaling Frequency
+            </Col>
+          </Row>
         </div>
       </div>
     </div>
